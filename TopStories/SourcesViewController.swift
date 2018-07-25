53af9168f83d7f89ea755a9bf856e891a6f658ac
Fleischauer, Joseph <jfleischauer@rlas-macair-09.local>
2018-07-25 11:09:29 -0500

    slide 3 commit

---
+++ TopStories/SourcesViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TopStories
+//
+//  Created by Fleischauer, Joseph on 7/25/18.
+//  Copyright © 2018 Fleischauer, Joseph. All rights reserved.
+//
+
+import UIKit
+
+class SourcesViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+   
+
+}
+
