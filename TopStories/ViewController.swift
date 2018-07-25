163a405293e5208453de5a4b947c45bbda2829fb
Fleischauer, Joseph <jfleischauer@rlas-macair-09.local>
2018-07-25 10:58:57 -0500

    Initial Commit

---
+++ TopStories/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TopStories
+//
+//  Created by Fleischauer, Joseph on 7/25/18.
+//  Copyright © 2018 Fleischauer, Joseph. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
